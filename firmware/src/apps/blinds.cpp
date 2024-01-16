9be667b00e3bc467a8d8df4f5f9183fbc1035fe4
carlhampuswall <carl.hampus.wall@gmail.com>
2024-01-16 11:33:56 +0100

    Commented out certain lines to work with new fixes, needs implementation instead.

--- firmware/src/apps/blinds.cpp
+++ firmware/src/apps/blinds.cpp
@@ -2,107 +2,107 @@
 
 BlindsApp::BlindsApp(TFT_eSprite *spr_, std::string entity_name) : App(spr_)
 {
-    this->entity_name = entity_name;
+  this->entity_name = entity_name;
 
-    motor_config = PB_SmartKnobConfig{
-        15,
-        0,
-        15,
-        0,
-        20,
-        8.225806452 * PI / 120,
-        2,
-        1,
-        1.1,
-        "SKDEMO_Shades",
-        0,
-        {},
-        0,
-        27,
-    };
+  motor_config = PB_SmartKnobConfig{
+      15,
+      0,
+      15,
+      0,
+      20,
+      8.225806452 * PI / 120,
+      2,
+      1,
+      1.1,
+      "SKDEMO_Shades",
+      0,
+      {},
+      0,
+      27,
+  };
 
-    big_icon = shades_80;
-    small_icon = shades_40;
-    friendly_name = "Shades";
+  big_icon = shades_80;
+  small_icon = shades_40;
+  friendly_name = "Shades";
 }
 
 uint8_t BlindsApp::navigationNext()
 {
-    // back to menu
-    return 0;
+  // back to menu
+  return 0;
 }
 
 EntityStateUpdate BlindsApp::updateStateFromKnob(PB_SmartKnobState state)
 {
-    current_closed_position = state.current_position;
+  current_closed_position = state.current_position;
 
-    // needed to next reload of App
-    motor_config.position_nonce = current_closed_position;
-    motor_config.position = current_closed_position;
+  // needed to next reload of App
+  motor_config.position_nonce = current_closed_position;
+  motor_config.position = current_closed_position;
 
-    EntityStateUpdate new_state;
+  EntityStateUpdate new_state;
 
-    new_state.entity_name = entity_name;
-    new_state.new_value = current_closed_position * 5.0; // 5 percent per position
+  // new_state.entity_name = entity_name;
+  // new_state.new_value = current_closed_position * 5.0; // 5 percent per position
 
-    if (last_closed_position != current_closed_position)
-    {
-        last_closed_position = current_closed_position;
-        new_state.changed = true;
-        sprintf(new_state.app_slug, "%s", APP_SLUG_BLINDS);
-    }
+  if (last_closed_position != current_closed_position)
+  {
+    last_closed_position = current_closed_position;
+    new_state.changed = true;
+    sprintf(new_state.app_slug, "%s", APP_SLUG_BLINDS);
+  }
 
-    return new_state;
+  return new_state;
 }
 
 void BlindsApp::updateStateFromSystem(AppState state) {}
 
 TFT_eSprite *BlindsApp::render()
 {
-    uint16_t DISABLED_COLOR = spr_->color565(71, 71, 71);
+  uint16_t DISABLED_COLOR = spr_->color565(71, 71, 71);
 
-    uint32_t light_background = spr_->color565(150, 150, 150);
+  uint32_t light_background = spr_->color565(150, 150, 150);
 
-    uint32_t shade_bar_color = TFT_OLIVE;
-    uint32_t shade_color = spr_->color565(50, 50, 50);
+  uint32_t shade_bar_color = TFT_OLIVE;
+  uint32_t shade_color = spr_->color565(50, 50, 50);
 
-    spr_->fillRect(0, 0, TFT_WIDTH, TFT_HEIGHT, light_background);
-    int32_t height = (current_closed_position - motor_config.min_position) * TFT_HEIGHT / (motor_config.max_position - motor_config.min_position);
-    spr_->fillRect(0, 0, TFT_WIDTH, height, shade_color);
-    spr_->fillRect(0, height - 10, TFT_WIDTH, 10, shade_bar_color);
+  spr_->fillRect(0, 0, TFT_WIDTH, TFT_HEIGHT, light_background);
+  int32_t height = (current_closed_position - motor_config.min_position) * TFT_HEIGHT / (motor_config.max_position - motor_config.min_position);
+  spr_->fillRect(0, 0, TFT_WIDTH, height, shade_color);
+  spr_->fillRect(0, height - 10, TFT_WIDTH, 10, shade_bar_color);
 
-    spr_->setFreeFont(&Roboto_Light_60);
+  spr_->setFreeFont(&Roboto_Light_60);
 
-    uint16_t center = TFT_WIDTH / 2;
+  uint16_t center = TFT_WIDTH / 2;
 
-    uint8_t arrow_size = 50;
+  uint8_t arrow_size = 50;
 
-    if (current_closed_position == 0)
-    {
-        sprintf(buf_, "%s", "Opened");
-        spr_->drawBitmap(center - arrow_size / 2, TFT_HEIGHT - 20 - arrow_size, arrow_down_50, arrow_size, arrow_size, shade_bar_color, light_background);
-    }
-    else if (current_closed_position == 10)
-    {
-        sprintf(buf_, "%s", "Half");
-    }
-    else if (current_closed_position == 20)
-    {
-        sprintf(buf_, "%s", "Closed");
-        spr_->drawBitmap(center - arrow_size / 2, TFT_HEIGHT - 20 - arrow_size, arrow_up_50, arrow_size, arrow_size, shade_bar_color, shade_color);
-    }
-    else
-    {
-        sprintf(buf_, "%d%%", current_closed_position * 5);
-        // spr_->drawBitmap(center - arrow_size / 2, TFT_HEIGHT - 20 - arrow_size, arrow_up_50, arrow_size, arrow_size, TFT_OLIVE, TFT_TRANSPARENT);
-    }
+  if (current_closed_position == 0)
+  {
+    sprintf(buf_, "%s", "Opened");
+    spr_->drawBitmap(center - arrow_size / 2, TFT_HEIGHT - 20 - arrow_size, arrow_down_50, arrow_size, arrow_size, shade_bar_color, light_background);
+  }
+  else if (current_closed_position == 10)
+  {
+    sprintf(buf_, "%s", "Half");
+  }
+  else if (current_closed_position == 20)
+  {
+    sprintf(buf_, "%s", "Closed");
+    spr_->drawBitmap(center - arrow_size / 2, TFT_HEIGHT - 20 - arrow_size, arrow_up_50, arrow_size, arrow_size, shade_bar_color, shade_color);
+  }
+  else
+  {
+    sprintf(buf_, "%d%%", current_closed_position * 5);
+    // spr_->drawBitmap(center - arrow_size / 2, TFT_HEIGHT - 20 - arrow_size, arrow_up_50, arrow_size, arrow_size, TFT_OLIVE, TFT_TRANSPARENT);
+  }
 
-    spr_->drawString(buf_, TFT_WIDTH / 2, TFT_HEIGHT / 2, 1);
+  spr_->drawString(buf_, TFT_WIDTH / 2, TFT_HEIGHT / 2, 1);
 
-    // spr_->drawNumber(current_closed_position, TFT_WIDTH / 2, TFT_HEIGHT / 2, 1);
-    spr_->setTextColor(TFT_WHITE);
-    spr_->setFreeFont(&Roboto_Thin_24);
-    spr_->drawString("Bedroom shade", TFT_WIDTH / 2, TFT_HEIGHT / 2 - DESCRIPTION_Y_OFFSET - VALUE_OFFSET, 1);
+  // spr_->drawNumber(current_closed_position, TFT_WIDTH / 2, TFT_HEIGHT / 2, 1);
+  spr_->setTextColor(TFT_WHITE);
+  spr_->setFreeFont(&Roboto_Thin_24);
+  spr_->drawString("Bedroom shade", TFT_WIDTH / 2, TFT_HEIGHT / 2 - DESCRIPTION_Y_OFFSET - VALUE_OFFSET, 1);
 
-    return this->spr_;
+  return this->spr_;
 };
--- firmware/src/apps/light_switch.cpp
+++ firmware/src/apps/light_switch.cpp
@@ -2,158 +2,158 @@
 
 LightSwitchApp::LightSwitchApp(TFT_eSprite *spr_, std::string entity_name) : App(spr_)
 {
-    // sprintf(author, "%s", "Beethoven");
-    // sprintf(track, "%s", "Moonlight Sonata");
-    this->entity_name = entity_name;
+  // sprintf(author, "%s", "Beethoven");
+  // sprintf(track, "%s", "Moonlight Sonata");
+  this->entity_name = entity_name;
 
-    motor_config = PB_SmartKnobConfig{
-        0,
-        0,
-        0,
-        0,
-        1,
-        60 * PI / 180,
-        1,
-        1,
-        0.55, // Note the snap point is slightly past the midpoint (0.5); compare to normal detents which use a snap point *past* the next value (i.e. > 1)
-        "SKDEMO_Light_switch",
-        0,
-        {},
-        0,
-        27,
-    };
+  motor_config = PB_SmartKnobConfig{
+      0,
+      0,
+      0,
+      0,
+      1,
+      60 * PI / 180,
+      1,
+      1,
+      0.55, // Note the snap point is slightly past the midpoint (0.5); compare to normal detents which use a snap point *past* the next value (i.e. > 1)
+      "SKDEMO_Light_switch",
+      0,
+      {},
+      0,
+      27,
+  };
 
-    friendly_name = "Lights";
+  friendly_name = "Lights";
 
-    big_icon = light_switch_80;
-    small_icon = light_switch_40;
+  big_icon = light_switch_80;
+  small_icon = light_switch_40;
 }
 
 uint8_t LightSwitchApp::navigationNext()
 {
-    // back to menu
-    return 0;
+  // back to menu
+  return 0;
 }
 
 EntityStateUpdate LightSwitchApp::updateStateFromKnob(PB_SmartKnobState state)
 {
-    current_position = state.current_position;
-    sub_position_unit = state.sub_position_unit;
-    // // needed to next reload of App
-    motor_config.position_nonce = current_position;
-    motor_config.position = current_position;
+  current_position = state.current_position;
+  sub_position_unit = state.sub_position_unit;
+  // // needed to next reload of App
+  motor_config.position_nonce = current_position;
+  motor_config.position = current_position;
 
-    adjusted_sub_position = sub_position_unit * motor_config.position_width_radians;
+  adjusted_sub_position = sub_position_unit * motor_config.position_width_radians;
 
-    if (state.current_position == motor_config.min_position && sub_position_unit < 0)
-    {
-        adjusted_sub_position = -logf(1 - sub_position_unit * motor_config.position_width_radians / 5 / PI * 180) * 5 * PI / 180;
-    }
-    else if (state.current_position == motor_config.max_position && sub_position_unit > 0)
-    {
-        adjusted_sub_position = logf(1 + sub_position_unit * motor_config.position_width_radians / 5 / PI * 180) * 5 * PI / 180;
-    }
+  if (state.current_position == motor_config.min_position && sub_position_unit < 0)
+  {
+    adjusted_sub_position = -logf(1 - sub_position_unit * motor_config.position_width_radians / 5 / PI * 180) * 5 * PI / 180;
+  }
+  else if (state.current_position == motor_config.max_position && sub_position_unit > 0)
+  {
+    adjusted_sub_position = logf(1 + sub_position_unit * motor_config.position_width_radians / 5 / PI * 180) * 5 * PI / 180;
+  }
 
-    EntityStateUpdate new_state;
+  EntityStateUpdate new_state;
 
-    new_state.entity_name = entity_name;
-    new_state.new_value = current_position * 1.0;
+  // new_state.entity_name = entity_name;
+  // new_state.new_value = current_position * 1.0;
 
-    if (last_position != current_position)
-    {
-        last_position = current_position;
-        new_state.changed = true;
-        sprintf(new_state.app_slug, "%s", APP_SLUG_LIGHT_SWITCH);
-    }
+  if (last_position != current_position)
+  {
+    last_position = current_position;
+    new_state.changed = true;
+    sprintf(new_state.app_slug, "%s", APP_SLUG_LIGHT_SWITCH);
+  }
 
-    return new_state;
+  return new_state;
 }
 
 void LightSwitchApp::updateStateFromSystem(AppState state) {}
 
 TFT_eSprite *LightSwitchApp::render()
 {
-    uint16_t DISABLED_COLOR = spr_->color565(71, 71, 71);
+  uint16_t DISABLED_COLOR = spr_->color565(71, 71, 71);
 
-    uint32_t off_background = spr_->color565(0, 0, 0);
-    uint32_t off_lamp_color = spr_->color565(150, 150, 150);
+  uint32_t off_background = spr_->color565(0, 0, 0);
+  uint32_t off_lamp_color = spr_->color565(150, 150, 150);
 
-    uint32_t on_background = spr_->color565(71, 39, 1);
-    uint32_t on_lamp_color = spr_->color565(245, 164, 66);
+  uint32_t on_background = spr_->color565(71, 39, 1);
+  uint32_t on_lamp_color = spr_->color565(245, 164, 66);
 
-    uint16_t center_h = TFT_WIDTH / 2;
-    uint16_t center_v = TFT_WIDTH / 2;
+  uint16_t center_h = TFT_WIDTH / 2;
+  uint16_t center_v = TFT_WIDTH / 2;
 
-    uint8_t icon_size = 80;
+  uint8_t icon_size = 80;
 
-    uint16_t offset_vertical = 20;
-    uint16_t screen_radius = TFT_WIDTH / 2;
+  uint16_t offset_vertical = 20;
+  uint16_t screen_radius = TFT_WIDTH / 2;
 
-    float left_bound = PI / 2;
-    float right_bound = 0;
-    float range_radians = (motor_config.max_position - motor_config.min_position) * motor_config.position_width_radians;
+  float left_bound = PI / 2;
+  float right_bound = 0;
+  float range_radians = (motor_config.max_position - motor_config.min_position) * motor_config.position_width_radians;
 
-    left_bound = PI / 2 + range_radians / 2;
-    right_bound = PI / 2 - range_radians / 2;
+  left_bound = PI / 2 + range_radians / 2;
+  right_bound = PI / 2 - range_radians / 2;
 
-    float raw_angle = left_bound - (current_position - motor_config.min_position) * motor_config.position_width_radians;
-    float adjusted_angle = raw_angle - adjusted_sub_position;
+  float raw_angle = left_bound - (current_position - motor_config.min_position) * motor_config.position_width_radians;
+  float adjusted_angle = raw_angle - adjusted_sub_position;
 
-    if (current_position == 0)
+  if (current_position == 0)
+  {
+    spr_->fillRect(0, 0, TFT_WIDTH, TFT_HEIGHT, off_background);
+    spr_->drawBitmap(center_h - icon_size / 2, center_v - icon_size / 2 - offset_vertical, lamp_regular, icon_size, icon_size, off_lamp_color, off_background);
+    spr_->setTextColor(off_lamp_color);
+    spr_->setFreeFont(&Roboto_Thin_24);
+    spr_->drawString("Kitchen", center_h, center_v + icon_size / 2 + 30 - offset_vertical, 1);
+  }
+  else
+  {
+    spr_->fillRect(0, 0, TFT_WIDTH, TFT_HEIGHT, on_background);
+    spr_->drawBitmap(center_h - icon_size / 2, center_v - icon_size / 2 - offset_vertical, lamp_solid, icon_size, icon_size, on_lamp_color, on_background);
+    spr_->setTextColor(on_lamp_color);
+    spr_->setFreeFont(&Roboto_Thin_24);
+    spr_->drawString("Kitchen", center_h, center_v + icon_size / 2 + 30 - offset_vertical, 1);
+  }
+
+  // set the moving dot color
+  uint32_t dot_color = TFT_WHITE;
+
+  if (current_position < 1)
+  {
+    dot_color = off_lamp_color;
+  }
+  else
+  {
+    dot_color = on_lamp_color;
+  }
+
+  // draw moving dot
+  if (num_positions > 0 && ((current_position == motor_config.min_position && sub_position_unit < 0) || (current_position == motor_config.max_position && sub_position_unit > 0)))
+  {
+
+    spr_->fillCircle(TFT_WIDTH / 2 + (screen_radius - 10) * cosf(raw_angle), TFT_HEIGHT / 2 - (screen_radius - 10) * sinf(raw_angle), 5, dot_color);
+    if (raw_angle < adjusted_angle)
     {
-        spr_->fillRect(0, 0, TFT_WIDTH, TFT_HEIGHT, off_background);
-        spr_->drawBitmap(center_h - icon_size / 2, center_v - icon_size / 2 - offset_vertical, lamp_regular, icon_size, icon_size, off_lamp_color, off_background);
-        spr_->setTextColor(off_lamp_color);
-        spr_->setFreeFont(&Roboto_Thin_24);
-        spr_->drawString("Kitchen", center_h, center_v + icon_size / 2 + 30 - offset_vertical, 1);
+      for (float r = raw_angle; r <= adjusted_angle; r += 2 * PI / 180)
+      {
+        spr_->fillCircle(TFT_WIDTH / 2 + (screen_radius - 10) * cosf(r), TFT_HEIGHT / 2 - (screen_radius - 10) * sinf(r), 2, dot_color);
+      }
+      spr_->fillCircle(TFT_WIDTH / 2 + (screen_radius - 10) * cosf(adjusted_angle), TFT_HEIGHT / 2 - (screen_radius - 10) * sinf(adjusted_angle), 2, dot_color);
     }
     else
     {
-        spr_->fillRect(0, 0, TFT_WIDTH, TFT_HEIGHT, on_background);
-        spr_->drawBitmap(center_h - icon_size / 2, center_v - icon_size / 2 - offset_vertical, lamp_solid, icon_size, icon_size, on_lamp_color, on_background);
-        spr_->setTextColor(on_lamp_color);
-        spr_->setFreeFont(&Roboto_Thin_24);
-        spr_->drawString("Kitchen", center_h, center_v + icon_size / 2 + 30 - offset_vertical, 1);
+      for (float r = raw_angle; r >= adjusted_angle; r -= 2 * PI / 180)
+      {
+        spr_->fillCircle(TFT_WIDTH / 2 + (screen_radius - 10) * cosf(r), TFT_HEIGHT / 2 - (screen_radius - 10) * sinf(r), 2, dot_color);
+      }
+      spr_->fillCircle(TFT_WIDTH / 2 + (screen_radius - 10) * cosf(adjusted_angle), TFT_HEIGHT / 2 - (screen_radius - 10) * sinf(adjusted_angle), 2, dot_color);
     }
+  }
+  else
+  {
+    spr_->fillCircle(TFT_WIDTH / 2 + (screen_radius - 10) * cosf(adjusted_angle), TFT_HEIGHT / 2 - (screen_radius - 10) * sinf(adjusted_angle), 5, dot_color);
+  }
 
-    // set the moving dot color
-    uint32_t dot_color = TFT_WHITE;
-
-    if (current_position < 1)
-    {
-        dot_color = off_lamp_color;
-    }
-    else
-    {
-        dot_color = on_lamp_color;
-    }
-
-    // draw moving dot
-    if (num_positions > 0 && ((current_position == motor_config.min_position && sub_position_unit < 0) || (current_position == motor_config.max_position && sub_position_unit > 0)))
-    {
-
-        spr_->fillCircle(TFT_WIDTH / 2 + (screen_radius - 10) * cosf(raw_angle), TFT_HEIGHT / 2 - (screen_radius - 10) * sinf(raw_angle), 5, dot_color);
-        if (raw_angle < adjusted_angle)
-        {
-            for (float r = raw_angle; r <= adjusted_angle; r += 2 * PI / 180)
-            {
-                spr_->fillCircle(TFT_WIDTH / 2 + (screen_radius - 10) * cosf(r), TFT_HEIGHT / 2 - (screen_radius - 10) * sinf(r), 2, dot_color);
-            }
-            spr_->fillCircle(TFT_WIDTH / 2 + (screen_radius - 10) * cosf(adjusted_angle), TFT_HEIGHT / 2 - (screen_radius - 10) * sinf(adjusted_angle), 2, dot_color);
-        }
-        else
-        {
-            for (float r = raw_angle; r >= adjusted_angle; r -= 2 * PI / 180)
-            {
-                spr_->fillCircle(TFT_WIDTH / 2 + (screen_radius - 10) * cosf(r), TFT_HEIGHT / 2 - (screen_radius - 10) * sinf(r), 2, dot_color);
-            }
-            spr_->fillCircle(TFT_WIDTH / 2 + (screen_radius - 10) * cosf(adjusted_angle), TFT_HEIGHT / 2 - (screen_radius - 10) * sinf(adjusted_angle), 2, dot_color);
-        }
-    }
-    else
-    {
-        spr_->fillCircle(TFT_WIDTH / 2 + (screen_radius - 10) * cosf(adjusted_angle), TFT_HEIGHT / 2 - (screen_radius - 10) * sinf(adjusted_angle), 5, dot_color);
-    }
-
-    return this->spr_;
+  return this->spr_;
 };
