4794d5890a580a4f367d8c95aec6d6f2f6adc3c8
Hampus Wall <31284797+carlhampuswall@users.noreply.github.com>
2024-02-03 10:57:48 +0100

    Onboarding HASS setup UI (#54)
    * Added seedlabs_logo
    
    * Added HASS setup onboarding screens/app

--- firmware/src/apps/onboarding/hass_setup.h
+++ firmware/src/apps/onboarding/hass_setup.h
@@ -1,5 +1,17 @@
 #include "../app.h"
 
+enum class HassSetupState
+{
+    WIFI_CONNECT,
+    KNOB_URL,
+    PROCESSING_WIFI,
+    NQTT_CONFIG,
+    CONTINUE_HASS,
+    DONE,
+    ERROR_WIFI,
+    ERROR_MQTT
+};
+
 class HassSetupApp : public App
 {
 public:
@@ -8,6 +20,21 @@ public:
     EntityStateUpdate updateStateFromKnob(PB_SmartKnobState state);
     void updateStateFromSystem(AppState state);
 
+    int8_t navigationNext();
+    int8_t navigationBack();
+
+protected:
 private:
-    void render_hass_setup_screen();
+    HassSetupState internal_state;
+
+    TFT_eSprite *renderWifiConnect();
+    TFT_eSprite *renderKnobUrl();
+    TFT_eSprite *renderProcessing();
+    TFT_eSprite *renderMQTTConfig();
+    TFT_eSprite *renderContinueHass();
+    TFT_eSprite *renderDone();
+    TFT_eSprite *renderErrorWifi();
+    TFT_eSprite *renderErrorMQTT();
+
+    unsigned long startup_ms;
 };
