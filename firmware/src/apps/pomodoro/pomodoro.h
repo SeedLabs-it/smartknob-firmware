56461ebc38252e54ccd7116181dbf468fd0e5432
Hampus Wall <31284797+carlhampuswall@users.noreply.github.com>
2024-02-04 20:37:52 +0100

    Added timer like arc countdown. (#59)

--- firmware/src/apps/pomodoro/pomodoro.h
+++ firmware/src/apps/pomodoro/pomodoro.h
@@ -1,6 +1,8 @@
 #include "../app.h"
 #include <chrono>
 
+#include "../../font/NDS1210pt7b.h"
+
 enum class PomodoroState
 {
     IDLE,
@@ -13,6 +15,7 @@ class PomodoroApp : public App
 public:
     PomodoroApp(TFT_eSprite *spr_);
     EntityStateUpdate update(AppState state);
+    EntityStateUpdate updateStateFromKnob(PB_SmartKnobState state);
 
     TFT_eSprite *render();
 
@@ -21,5 +24,6 @@ protected:
 
 private:
     PomodoroState state;
-    long startTime;
+    boolean state_changed = false;
+    long start_time;
 };
