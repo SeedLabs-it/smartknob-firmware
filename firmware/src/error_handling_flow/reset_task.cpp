f6277a77a5b50a20e0f3236e3e161c4149a30db4
Hampus Wall <carl.hampus.wall@gmail.com>
2024-03-27 19:34:33 +0100

    Changed reset time from 4s to 10s

--- firmware/src/error_handling_flow/reset_task.cpp
+++ firmware/src/error_handling_flow/reset_task.cpp
@@ -1,5 +1,7 @@
 #include "reset_task.h"
 
+#define RESET_BUTTON GPIO_NUM_0
+
 ResetTask::ResetTask(const uint8_t task_core) : Task("ResetTask", 2048, task_core)
 {
 }
@@ -10,20 +12,17 @@ ResetTask::~ResetTask()
 
 void ResetTask::run()
 {
-    // esp_restart();
-    long button_pressed;
+    long reset_button_pressed;
     while (1)
     {
-        // ESP_LOGD("ResetTask", "ResetTask!!!!");
-        if (gpio_get_level(GPIO_NUM_0) == 1)
+        if (gpio_get_level(RESET_BUTTON) == 1)
         {
-            button_pressed = millis();
+            reset_button_pressed = millis();
         }
         else
         {
-            if (millis() - button_pressed > RESET_SECONDS * 1000)
+            if (millis() - reset_button_pressed > RESET_SECONDS * 1000)
             {
-                ESP_LOGD("ResetTask", "Resetting!!!!");
                 esp_partition_iterator_t it = esp_partition_find(ESP_PARTITION_TYPE_APP, ESP_PARTITION_SUBTYPE_APP_OTA_0, NULL);
 
                 if (it != NULL)
@@ -32,12 +31,10 @@ void ResetTask::run()
                     esp_partition_iterator_release(it);
                     if (esp_ota_set_boot_partition(factory) == ESP_OK)
                     {
-                        ESP_LOGE("ResetTask", "Set boot partition");
                         esp_restart();
                     }
                     else
                     {
-                        ESP_LOGE("ResetTask", "Failed to set boot partition");
                     }
                 }
             }
