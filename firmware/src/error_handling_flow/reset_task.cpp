0b017e3fee26f68a455d414a5d9bb78519d2cb6f
Hampus Wall <carl.hampus.wall@gmail.com>
2024-03-28 21:25:19 +0100

    Moved hard reset to 6 quick presses in short succession (500ms max inbetween). Soft reset acheived by hoding boot button for 5s.

--- firmware/src/error_handling_flow/reset_task.cpp
+++ firmware/src/error_handling_flow/reset_task.cpp
@@ -13,53 +13,91 @@ ResetTask::~ResetTask()
 void ResetTask::run()
 {
     long reset_button_pressed;
+    long reset_button_released;
+    bool held = false;
+    uint8_t pressedCount = 0;
+
     while (1)
     {
+        // Button is not pressed.
         if (gpio_get_level(RESET_BUTTON) == 1)
         {
             reset_button_pressed = millis();
+            if (held)
+            {
+                pressedCount += 1;
+                held = false;
+                ESP_LOGD("", "Button pressed %d times", pressedCount);
+            }
         }
-        else
+        else // Button is pressed when low (0)
         {
+            reset_button_released = millis();
+            if (!held)
+            {
+                held = true;
+                ESP_LOGD("", "Button held");
+            }
+
             if (millis() - reset_button_pressed > RESET_SECONDS * 1000)
             {
-                esp_partition_iterator_t it = esp_partition_find(ESP_PARTITION_TYPE_APP, ESP_PARTITION_SUBTYPE_APP_OTA_0, NULL);
-
-                if (it != NULL)
+                log("Resetting to factory defaults");
+                if (configuration_.resetToDefaults())
                 {
-                    const esp_partition_t *factory = esp_partition_get(it);
-                    esp_partition_iterator_release(it);
-                    if (esp_ota_set_boot_partition(factory) == ESP_OK)
+                    log("Configuration reset to defaults");
+                    esp_restart();
+                }
+                else
+                {
+                    log("Failed to reset configuration to defaults");
+                }
+            }
+        }
+
+        if (pressedCount >= 6)
+        {
+            esp_partition_iterator_t it = esp_partition_find(ESP_PARTITION_TYPE_APP, ESP_PARTITION_SUBTYPE_APP_OTA_0, NULL);
+
+            if (it != NULL)
+            {
+                const esp_partition_t *factory = esp_partition_get(it);
+                esp_partition_iterator_release(it);
+                if (esp_ota_set_boot_partition(factory) == ESP_OK)
+                {
+                    log("FACTORY RESETTING");
+                    if (configuration_.resetToDefaults())
                     {
-                        log("FACTORY RESETTING");
-                        if (configuration_.resetToDefaults())
-                        {
-                            log("Configuration reset to defaults");
-                            esp_restart();
-                        }
-                        else
-                        {
-                            log("Failed to reset configuration to defaults");
-                        }
+                        log("Configuration reset to defaults");
+                        esp_restart();
                     }
                     else
                     {
-                        if (verbose_)
-                        {
-                            log("Failed to set boot partition");
-                        }
+                        log("Failed to reset configuration to defaults");
                     }
                 }
                 else
                 {
                     if (verbose_)
                     {
-                        log("Failed to find factory partition");
+                        log("Failed to set boot partition");
                     }
                 }
             }
+            else
+            {
+                if (verbose_)
+                {
+                    log("Failed to find factory partition");
+                }
+            }
         }
-        vTaskDelay(1000 / portTICK_PERIOD_MS);
+
+        if (millis() - reset_button_released > 500) // Reset the pressed count after 500ms of no button press
+        {
+            pressedCount = 0;
+        }
+
+        vTaskDelay(10 / portTICK_PERIOD_MS);
     }
 }
 
