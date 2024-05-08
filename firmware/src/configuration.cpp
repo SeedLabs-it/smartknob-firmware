SeedLabs-it/smartknob-firmware

commit 288b8e4e0d924315566bd74a9fc1b552bdd62041

Knob info for web app. (#117)

firmware/src/configuration.cpp
@@ -101,40 +101,49 @@ bool Configuration::loadFromDisk()
 
 bool Configuration::saveToDisk()
 {
-    SemaphoreGuard lock(mutex_);
-
-    pb_ostream_t stream = pb_ostream_from_buffer(buffer_, sizeof(buffer_));
-    pb_buffer_.version = PERSISTENT_CONFIGURATION_VERSION;
-    if (!pb_encode(&stream, PB_PersistentConfiguration_fields, &pb_buffer_))
     {
-        char buf_[200];
-        snprintf(buf_, sizeof(buf_), "Encoding failed: %s", PB_GET_ERROR(&stream));
-        LOGE(buf_);
-        return false;
+        SemaphoreGuard lock(mutex_);
+
+        pb_ostream_t stream = pb_ostream_from_buffer(buffer_, sizeof(buffer_));
+        pb_buffer_.version = PERSISTENT_CONFIGURATION_VERSION;
+        if (!pb_encode(&stream, PB_PersistentConfiguration_fields, &pb_buffer_))
+        {
+            char buf_[200];
+            snprintf(buf_, sizeof(buf_), "Encoding failed: %s", PB_GET_ERROR(&stream));
+            LOGE(buf_);
+            return false;
+        }
+
+        FatGuard fatGuard;
+        if (!fatGuard.mounted_)
+        {
+            return false;
+        }
+
+        File f = FFat.open(CONFIG_PATH, FILE_WRITE);
+        if (!f)
+        {
+            LOGE("Failed to read config file");
+            return false;
+        }
+
+        size_t written = f.write(buffer_, stream.bytes_written);
+        f.close();
+
+        LOGD("Saved config. Wrote %d bytes", written);
+
+        if (written != stream.bytes_written)
+        {
+            LOGE("Failed to write all bytes to file");
+            return false;
+        }
     }
 
-    FatGuard fatGuard;
-    if (!fatGuard.mounted_)
+    if (shared_events_queue != NULL)
     {
-        return false;
-    }
-
-    File f = FFat.open(CONFIG_PATH, FILE_WRITE);
-    if (!f)
-    {
-        LOGE("Failed to read config file");
-        return false;
-    }
-
-    size_t written = f.write(buffer_, stream.bytes_written);
-    f.close();
-
-    LOGD("Saved config. Wrote %d bytes", written);
-
-    if (written != stream.bytes_written)
-    {
-        LOGE("Failed to write all bytes to file");
-        return false;
+        WiFiEvent event;
+        event.type = SK_CONFIGURATION_SAVED;
+        publishEvent(event);
     }
 
     return true;
@@ -290,4 +299,15 @@ bool Configuration::setMotorCalibrationAndSave(PB_MotorCalibration &motor_calibr
         pb_buffer_.has_motor = true;
     }
     return saveToDisk();
+}
+
+void Configuration::setSharedEventsQueue(QueueHandle_t shared_events_queue)
+{
+    this->shared_events_queue = shared_events_queue;
+}
+
+void Configuration::publishEvent(WiFiEvent event)
+{
+    event.sent_at = millis();
+    xQueueSendToBack(shared_events_queue, &event, 0);
 }

firmware/src/main.cpp
@@ -27,7 +27,7 @@ static LedRingTask *led_ring_task_p = &led_ring_task;
 static LedRingTask *led_ring_task_p = nullptr;
 #endif
 
-static MotorTask motor_task(1, config);
+static MotorTask motor_task(0, config);
 
 #if SK_WIFI
 static WifiTask wifi_task(1);
@@ -45,13 +45,13 @@ static MqttTask *mqtt_task_p = nullptr;
 
 #endif
 
-static SensorsTask sensors_task(1, &config);
+static SensorsTask sensors_task(0, &config);
 static SensorsTask *sensors_task_p = &sensors_task;
 
 static ResetTask reset_task(1, config);
 static ResetTask *reset_task_p = &reset_task;
 
-RootTask root_task(0, motor_task, display_task_p, wifi_task_p, mqtt_task_p, led_ring_task_p, sensors_task_p, reset_task_p);
+RootTask root_task(0, &config, motor_task, display_task_p, wifi_task_p, mqtt_task_p, led_ring_task_p, sensors_task_p, reset_task_p);
 
 void initTempSensor()
 {
@@ -95,7 +95,7 @@ void setup()
         config.saveToDisk();
     }
 
-    root_task.setConfiguration(&config);
+    root_task.loadConfiguration();
 
     motor_task.begin();
 

firmware/src/root_task.cpp
@@ -14,6 +14,7 @@ void delete_me_TriggerMotorCalibration()
 
 RootTask::RootTask(
     const uint8_t task_core,
+    Configuration *configuration,
     MotorTask &motor_task,
     DisplayTask *display_task,
     WifiTask *wifi_task,
@@ -22,6 +23,7 @@ RootTask::RootTask(
     SensorsTask *sensors_task,
     ResetTask *reset_task) : Task("RootTask", 1024 * 16, ESP_TASK_MAIN_PRIO, task_core),
                              stream_(),
+                             configuration_(configuration),
                              motor_task_(motor_task),
                              display_task_(display_task),
                              wifi_task_(wifi_task),
@@ -32,20 +34,21 @@ RootTask::RootTask(
                              plaintext_protocol_(
                                  stream_, [this]()
                                  { motor_task_.runCalibration(); },
-                                 [this]()
-                                 { sensors_task_->factoryStrainCalibrationCallback(); },
+                                 [this](float calibration_weight)
+                                 { sensors_task_->factoryStrainCalibrationCallback(calibration_weight); },
                                  [this]()
                                  {
                                      sensors_task_->weightMeasurementCallback();
                                  }),
                              proto_protocol_(
                                  stream_,
+                                 configuration,
                                  [this](PB_SmartKnobConfig &config)
                                  { applyConfig(config, true); },
                                  [this]()
                                  { motor_task_.runCalibration(); },
-                                 [this]()
-                                 { sensors_task_->factoryStrainCalibrationCallback(); })
+                                 [this](float calibration_weight)
+                                 { sensors_task_->factoryStrainCalibrationCallback(calibration_weight); })
 
 {
 #if SK_DISPLAY
@@ -185,6 +188,10 @@ void RootTask::run()
         xSemaphoreGive(mutex_);
         vTaskDelay(pdMS_TO_TICKS(50));
     }
+
+    configuration_->setSharedEventsQueue(wifi_task_->getWiFiEventsQueue());
+
+    sensors_task_->setSharedEventsQueue(wifi_task_->getWiFiEventsQueue());
 
     reset_task_->setSharedEventsQueue(wifi_task_->getWiFiEventsQueue());
 
@@ -341,7 +348,19 @@ void RootTask::run()
                 wifi_task_->retryMqtt(true); //! SUPER UGLY FIX/HACK, NEEDED TO REDIRECT USER IF MQTT CREDENTIALS FAILED
                 // wifi_task_->getNotifier()->requestRetryMQTT(); //! DOESNT WORK WITH NOTIFIER, NEEDS TO UPDATE BOOL, BUT WIFI_TASK IS IN LOOP WAITING FOR THIS BOOL TO CHANGE
                 break;
-
+            case SK_CONFIGURATION_SAVED:
+                if (current_protocol_ == &proto_protocol_)
+                {
+                    proto_protocol_.sendInitialInfo();
+                }
+                break;
+            case SK_STRAIN_CALIBRATION:
+                if (current_protocol_ == &proto_protocol_)
+                {
+                    LOGD("Sending strain calib state.");
+                    proto_protocol_.sendStrainCalibState(wifi_event.body.calibration_step);
+                }
+                break;
             default:
                 mqtt_task_->handleEvent(wifi_event);
                 break;
@@ -678,10 +697,9 @@ void RootTask::updateHardware(AppState app_state)
     // #endif
 }
 
-void RootTask::setConfiguration(Configuration *configuration)
+void RootTask::loadConfiguration()
 {
     SemaphoreGuard lock(mutex_);
-    configuration_ = configuration;
     if (!configuration_loaded_)
     {
         if (configuration_ != nullptr)

firmware/src/configuration.h
@@ -10,6 +10,7 @@
 #include "proto_gen/smartknob.pb.h"
 
 #include "EEPROM.h"
+#include "./events/events.h"
 
 // TODO: should move these consts to wifi?
 static const uint16_t WIFI_SSID_LENGTH = 128;
@@ -53,15 +54,6 @@ struct WiFiConfiguration
     char knob_id[64];
 };
 
-struct MQTTConfiguration
-{
-    char host[64];
-    uint16_t port;
-    char user[64];
-    char password[64];
-    char knob_id[64];
-};
-
 enum OSMode
 {
     Onboarding = 0,
@@ -98,8 +90,13 @@ public:
     OSConfiguration *getOSConfiguration();
     const char *getKnobId();
 
+    void setSharedEventsQueue(QueueHandle_t shared_event_queue);
+    void publishEvent(WiFiEvent event);
+
 private:
     SemaphoreHandle_t mutex_;
+
+    QueueHandle_t shared_events_queue;
 
     bool loaded_ = false;
     PB_PersistentConfiguration pb_buffer_ = {};

firmware/src/events/events.h
@@ -1,7 +1,6 @@
 #pragma once
 
 #include <stdint.h>
-#include "configuration.h"
 
 struct WiFiAPStarted
 {
@@ -44,6 +43,14 @@ struct WebClient
 //     uint8_t sta_signal_strenth_status;
 //     int8_t sta_rssi;
 // };
+struct MQTTConfiguration
+{
+    char host[64];
+    uint16_t port;
+    char user[64];
+    char password[64];
+    char knob_id[64];
+};
 
 struct MQTTStateUpdate
 {
@@ -94,6 +101,7 @@ union WiFiEventBody
     MQTTConfiguration mqtt_connecting;
     MQTTStateUpdate mqtt_state_update;
     Error error;
+    uint8_t calibration_step;
 };
 
 // TODO, think events more careful, for example add SK_MQTT_CREDENTIALS_RECIEVED
@@ -136,6 +144,10 @@ enum EventType
 
     SK_MQTT_ERROR,
     SK_WIFI_ERROR,
+
+    SK_CONFIGURATION_SAVED,
+
+    SK_STRAIN_CALIBRATION
 };
 
 typedef unsigned long SentAt;

firmware/src/interface_callbacks.h
@@ -6,6 +6,6 @@
 
 typedef std::function<void(PB_SmartKnobConfig &)> ConfigCallback;
 typedef std::function<void(void)> MotorCalibrationCallback;
-typedef std::function<void(void)> StrainCalibrationCallback;
-typedef std::function<void(void)> FactoryStrainCalibrationCallback;
+typedef std::function<void(float)> StrainCalibrationCallback;
+typedef std::function<void(float)> FactoryStrainCalibrationCallback;
 typedef std::function<void(void)> WeightMeasurementCallback;

firmware/src/proto_gen/smartknob.pb.c
@@ -12,6 +12,15 @@ PB_BIND(PB_FromSmartKnob, PB_FromSmartKnob, 2)
 PB_BIND(PB_ToSmartknob, PB_ToSmartknob, AUTO)
 
 
+PB_BIND(PB_Knob, PB_Knob, AUTO)
+
+
+PB_BIND(PB_MotorCalibState, PB_MotorCalibState, AUTO)
+
+
+PB_BIND(PB_StrainCalibState, PB_StrainCalibState, AUTO)
+
+
 PB_BIND(PB_Ack, PB_Ack, AUTO)
 
 
@@ -31,6 +40,12 @@ PB_BIND(PB_PersistentConfiguration, PB_PersistentConfiguration, AUTO)
 
 
 PB_BIND(PB_MotorCalibration, PB_MotorCalibration, AUTO)
+
+
+PB_BIND(PB_StrainState, PB_StrainState, AUTO)
+
+
+PB_BIND(PB_StrainCalibration, PB_StrainCalibration, AUTO)
 
 
 

firmware/src/proto_gen/smartknob.pb.h
@@ -19,11 +19,23 @@ typedef enum _PB_LogLevel {
 } PB_LogLevel;
 
 typedef enum _PB_SmartKnobCommand {
-    PB_SmartKnobCommand_MOTOR_CALIBRATE = 0,
-    PB_SmartKnobCommand_STRAIN_CALIBRATE = 1
+    PB_SmartKnobCommand_GET_KNOB_INFO = 0,
+    PB_SmartKnobCommand_MOTOR_CALIBRATE = 1,
+    PB_SmartKnobCommand_STRAIN_CALIBRATE = 2
 } PB_SmartKnobCommand;
 
 /* Struct definitions */
+/* * Motor calibration state information */
+typedef struct _PB_MotorCalibState {
+    bool calibrated; /* * PLACEHOLDER */
+} PB_MotorCalibState;
+
+/* * Strain calibration state information */
+typedef struct _PB_StrainCalibState {
+    uint32_t step;
+    float strain_scale;
+} PB_StrainCalibState;
+
 /* * Lets the host know that a ToSmartknob message was received and should not be retried. */
 typedef struct _PB_Ack {
     uint32_t nonce;
@@ -170,33 +182,9 @@ typedef struct _PB_SmartKnobState {
     uint8_t press_nonce;
 } PB_SmartKnobState;
 
-/* Message FROM the SmartKnob to the host */
-typedef struct _PB_FromSmartKnob {
-    uint8_t protocol_version;
-    char mac_address[51];
-    pb_size_t which_payload;
-    union {
-        PB_Ack ack;
-        PB_Log log;
-        PB_SmartKnobState smartknob_state;
-    } payload;
-} PB_FromSmartKnob;
-
 typedef struct _PB_RequestState {
     char dummy_field;
 } PB_RequestState;
-
-/* Message TO the Smartknob from the host */
-typedef struct _PB_ToSmartknob {
-    uint8_t protocol_version;
-    uint32_t nonce;
-    pb_size_t which_payload;
-    union {
-        PB_RequestState request_state;
-        PB_SmartKnobConfig smartknob_config;
-        PB_SmartKnobCommand smartknob_command;
-    } payload;
-} PB_ToSmartknob;
 
 typedef struct _PB_MotorCalibration {
     bool calibrated;
@@ -212,6 +200,50 @@ typedef struct _PB_PersistentConfiguration {
     float strain_scale;
 } PB_PersistentConfiguration;
 
+/* * Initial knob information. */
+typedef struct _PB_Knob {
+    char mac_address[51];
+    char ip_address[51];
+    bool has_persistent_config;
+    PB_PersistentConfiguration persistent_config;
+} PB_Knob;
+
+/* Message FROM the SmartKnob to the host */
+typedef struct _PB_FromSmartKnob {
+    uint8_t protocol_version;
+    pb_size_t which_payload;
+    union {
+        PB_Knob knob;
+        PB_Ack ack;
+        PB_Log log;
+        PB_SmartKnobState smartknob_state;
+        PB_MotorCalibState motor_calib_state;
+        PB_StrainCalibState strain_calib_state;
+    } payload;
+} PB_FromSmartKnob;
+
+typedef struct _PB_StrainState {
+    int32_t press_weight;
+    float press_value;
+} PB_StrainState;
+
+typedef struct _PB_StrainCalibration {
+    float calibration_weight;
+} PB_StrainCalibration;
+
+/* Message TO the Smartknob from the host */
+typedef struct _PB_ToSmartknob {
+    uint8_t protocol_version;
+    uint32_t nonce;
+    pb_size_t which_payload;
+    union {
+        PB_RequestState request_state;
+        PB_SmartKnobConfig smartknob_config;
+        PB_SmartKnobCommand smartknob_command;
+        PB_StrainCalibration strain_calibration;
+    } payload;
+} PB_ToSmartknob;
+
 
 #ifdef __cplusplus
 extern "C" {
@@ -222,12 +254,15 @@ extern "C" {
 #define _PB_LogLevel_MAX PB_LogLevel_VERBOSE
 #define _PB_LogLevel_ARRAYSIZE ((PB_LogLevel)(PB_LogLevel_VERBOSE+1))
 
-#define _PB_SmartKnobCommand_MIN PB_SmartKnobCommand_MOTOR_CALIBRATE
+#define _PB_SmartKnobCommand_MIN PB_SmartKnobCommand_GET_KNOB_INFO
 #define _PB_SmartKnobCommand_MAX PB_SmartKnobCommand_STRAIN_CALIBRATE
 #define _PB_SmartKnobCommand_ARRAYSIZE ((PB_SmartKnobCommand)(PB_SmartKnobCommand_STRAIN_CALIBRATE+1))
 
 
 #define PB_ToSmartknob_payload_smartknob_command_ENUMTYPE PB_SmartKnobCommand
+
+
+
 
 
 #define PB_Log_level_ENUMTYPE PB_LogLevel
@@ -238,9 +273,14 @@ extern "C" {
 
 
 
+
+
 /* Initializer values for message structs */
-#define PB_FromSmartKnob_init_default            {0, "", 0, {PB_Ack_init_default}}
+#define PB_FromSmartKnob_init_default            {0, 0, {PB_Knob_init_default}}
 #define PB_ToSmartknob_init_default              {0, 0, 0, {PB_RequestState_init_default}}
+#define PB_Knob_init_default                     {"", "", false, PB_PersistentConfiguration_init_default}
+#define PB_MotorCalibState_init_default          {0}
+#define PB_StrainCalibState_init_default         {0, 0}
 #define PB_Ack_init_default                      {0}
 #define PB_Log_init_default                      {"", _PB_LogLevel_MIN, "", 0}
 #define PB_SmartKnobState_init_default           {0, 0, false, PB_SmartKnobConfig_init_default, 0}
@@ -248,8 +288,13 @@ extern "C" {
 #define PB_RequestState_init_default             {0}
 #define PB_PersistentConfiguration_init_default  {0, false, PB_MotorCalibration_init_default, 0}
 #define PB_MotorCalibration_init_default         {0, 0, 0, 0}
-#define PB_FromSmartKnob_init_zero               {0, "", 0, {PB_Ack_init_zero}}
+#define PB_StrainState_init_default              {0, 0}
+#define PB_StrainCalibration_init_default        {0}
+#define PB_FromSmartKnob_init_zero               {0, 0, {PB_Knob_init_zero}}
 #define PB_ToSmartknob_init_zero                 {0, 0, 0, {PB_RequestState_init_zero}}
+#define PB_Knob_init_zero                        {"", "", false, PB_PersistentConfiguration_init_zero}
+#define PB_MotorCalibState_init_zero             {0}
+#define PB_StrainCalibState_init_zero            {0, 0}
 #define PB_Ack_init_zero                         {0}
 #define PB_Log_init_zero                         {"", _PB_LogLevel_MIN, "", 0}
 #define PB_SmartKnobState_init_zero              {0, 0, false, PB_SmartKnobConfig_init_zero, 0}
@@ -257,8 +302,13 @@ extern "C" {
 #define PB_RequestState_init_zero                {0}
 #define PB_PersistentConfiguration_init_zero     {0, false, PB_MotorCalibration_init_zero, 0}
 #define PB_MotorCalibration_init_zero            {0, 0, 0, 0}
+#define PB_StrainState_init_zero                 {0, 0}
+#define PB_StrainCalibration_init_zero           {0}
 
 /* Field tags (for use in manual encoding/decoding) */
+#define PB_MotorCalibState_calibrated_tag        1
+#define PB_StrainCalibState_step_tag             1
+#define PB_StrainCalibState_strain_scale_tag     2
 #define PB_Ack_nonce_tag                         1
 #define PB_Log_msg_tag                           1
 #define PB_Log_level_tag                         2
@@ -281,16 +331,6 @@ extern "C" {
 #define PB_SmartKnobState_sub_position_unit_tag  2
 #define PB_SmartKnobState_config_tag             3
 #define PB_SmartKnobState_press_nonce_tag        4
-#define PB_FromSmartKnob_protocol_version_tag    1
-#define PB_FromSmartKnob_mac_address_tag         2
-#define PB_FromSmartKnob_ack_tag                 3
-#define PB_FromSmartKnob_log_tag                 4
-#define PB_FromSmartKnob_smartknob_state_tag     5
-#define PB_ToSmartknob_protocol_version_tag      1
-#define PB_ToSmartknob_nonce_tag                 2
-#define PB_ToSmartknob_request_state_tag         3
-#define PB_ToSmartknob_smartknob_config_tag      4
-#define PB_ToSmartknob_smartknob_command_tag     5
 #define PB_MotorCalibration_calibrated_tag       1
 #define PB_MotorCalibration_zero_electrical_offset_tag 2
 #define PB_MotorCalibration_direction_cw_tag     3
@@ -298,30 +338,75 @@ extern "C" {
 #define PB_PersistentConfiguration_version_tag   1
 #define PB_PersistentConfiguration_motor_tag     2
 #define PB_PersistentConfiguration_strain_scale_tag 3
+#define PB_Knob_mac_address_tag                  1
+#define PB_Knob_ip_address_tag                   2
+#define PB_Knob_persistent_config_tag            3
+#define PB_FromSmartKnob_protocol_version_tag    1
+#define PB_FromSmartKnob_knob_tag                3
+#define PB_FromSmartKnob_ack_tag                 4
+#define PB_FromSmartKnob_log_tag                 5
+#define PB_FromSmartKnob_smartknob_state_tag     6
+#define PB_FromSmartKnob_motor_calib_state_tag   7
+#define PB_FromSmartKnob_strain_calib_state_tag  8
+#define PB_StrainState_press_weight_tag          1
+#define PB_StrainState_press_value_tag           2
+#define PB_StrainCalibration_calibration_weight_tag 1
+#define PB_ToSmartknob_protocol_version_tag      1
+#define PB_ToSmartknob_nonce_tag                 2
+#define PB_ToSmartknob_request_state_tag         3
+#define PB_ToSmartknob_smartknob_config_tag      4
+#define PB_ToSmartknob_smartknob_command_tag     5
+#define PB_ToSmartknob_strain_calibration_tag    6
 
 /* Struct field encoding specification for nanopb */
 #define PB_FromSmartKnob_FIELDLIST(X, a) \
 X(a, STATIC,   SINGULAR, UINT32,   protocol_version,   1) \
-X(a, STATIC,   SINGULAR, STRING,   mac_address,       2) \
-X(a, STATIC,   ONEOF,    MESSAGE,  (payload,ack,payload.ack),   3) \
-X(a, STATIC,   ONEOF,    MESSAGE,  (payload,log,payload.log),   4) \
-X(a, STATIC,   ONEOF,    MESSAGE,  (payload,smartknob_state,payload.smartknob_state),   5)
+X(a, STATIC,   ONEOF,    MESSAGE,  (payload,knob,payload.knob),   3) \
+X(a, STATIC,   ONEOF,    MESSAGE,  (payload,ack,payload.ack),   4) \
+X(a, STATIC,   ONEOF,    MESSAGE,  (payload,log,payload.log),   5) \
+X(a, STATIC,   ONEOF,    MESSAGE,  (payload,smartknob_state,payload.smartknob_state),   6) \
+X(a, STATIC,   ONEOF,    MESSAGE,  (payload,motor_calib_state,payload.motor_calib_state),   7) \
+X(a, STATIC,   ONEOF,    MESSAGE,  (payload,strain_calib_state,payload.strain_calib_state),   8)
 #define PB_FromSmartKnob_CALLBACK NULL
 #define PB_FromSmartKnob_DEFAULT NULL
+#define PB_FromSmartKnob_payload_knob_MSGTYPE PB_Knob
 #define PB_FromSmartKnob_payload_ack_MSGTYPE PB_Ack
 #define PB_FromSmartKnob_payload_log_MSGTYPE PB_Log
 #define PB_FromSmartKnob_payload_smartknob_state_MSGTYPE PB_SmartKnobState
+#define PB_FromSmartKnob_payload_motor_calib_state_MSGTYPE PB_MotorCalibState
+#define PB_FromSmartKnob_payload_strain_calib_state_MSGTYPE PB_StrainCalibState
 
 #define PB_ToSmartknob_FIELDLIST(X, a) \
 X(a, STATIC,   SINGULAR, UINT32,   protocol_version,   1) \
 X(a, STATIC,   SINGULAR, UINT32,   nonce,             2) \
 X(a, STATIC,   ONEOF,    MESSAGE,  (payload,request_state,payload.request_state),   3) \
 X(a, STATIC,   ONEOF,    MESSAGE,  (payload,smartknob_config,payload.smartknob_config),   4) \
-X(a, STATIC,   ONEOF,    UENUM,    (payload,smartknob_command,payload.smartknob_command),   5)
+X(a, STATIC,   ONEOF,    UENUM,    (payload,smartknob_command,payload.smartknob_command),   5) \
+X(a, STATIC,   ONEOF,    MESSAGE,  (payload,strain_calibration,payload.strain_calibration),   6)
 #define PB_ToSmartknob_CALLBACK NULL
 #define PB_ToSmartknob_DEFAULT NULL
 #define PB_ToSmartknob_payload_request_state_MSGTYPE PB_RequestState
 #define PB_ToSmartknob_payload_smartknob_config_MSGTYPE PB_SmartKnobConfig
+#define PB_ToSmartknob_payload_strain_calibration_MSGTYPE PB_StrainCalibration
+
+#define PB_Knob_FIELDLIST(X, a) \
+X(a, STATIC,   SINGULAR, STRING,   mac_address,       1) \
+X(a, STATIC,   SINGULAR, STRING,   ip_address,        2) \
+X(a, STATIC,   OPTIONAL, MESSAGE,  persistent_config,   3)
+#define PB_Knob_CALLBACK NULL
+#define PB_Knob_DEFAULT NULL
+#define PB_Knob_persistent_config_MSGTYPE PB_PersistentConfiguration
+
+#define PB_MotorCalibState_FIELDLIST(X, a) \
+X(a, STATIC,   SINGULAR, BOOL,     calibrated,        1)
+#define PB_MotorCalibState_CALLBACK NULL
+#define PB_MotorCalibState_DEFAULT NULL
+
+#define PB_StrainCalibState_FIELDLIST(X, a) \
+X(a, STATIC,   SINGULAR, UINT32,   step,              1) \
+X(a, STATIC,   SINGULAR, FLOAT,    strain_scale,      2)
+#define PB_StrainCalibState_CALLBACK NULL
+#define PB_StrainCalibState_DEFAULT NULL
 
 #define PB_Ack_FIELDLIST(X, a) \
 X(a, STATIC,   SINGULAR, UINT32,   nonce,             1)
@@ -383,8 +468,22 @@ X(a, STATIC,   SINGULAR, UINT32,   pole_pairs,        4)
 #define PB_MotorCalibration_CALLBACK NULL
 #define PB_MotorCalibration_DEFAULT NULL
 
+#define PB_StrainState_FIELDLIST(X, a) \
+X(a, STATIC,   SINGULAR, INT32,    press_weight,      1) \
+X(a, STATIC,   SINGULAR, FLOAT,    press_value,       2)
+#define PB_StrainState_CALLBACK NULL
+#define PB_StrainState_DEFAULT NULL
+
+#define PB_StrainCalibration_FIELDLIST(X, a) \
+X(a, STATIC,   SINGULAR, FLOAT,    calibration_weight,   1)
+#define PB_StrainCalibration_CALLBACK NULL
+#define PB_StrainCalibration_DEFAULT NULL
+
 extern const pb_msgdesc_t PB_FromSmartKnob_msg;
 extern const pb_msgdesc_t PB_ToSmartknob_msg;
+extern const pb_msgdesc_t PB_Knob_msg;
+extern const pb_msgdesc_t PB_MotorCalibState_msg;
+extern const pb_msgdesc_t PB_StrainCalibState_msg;
 extern const pb_msgdesc_t PB_Ack_msg;
 extern const pb_msgdesc_t PB_Log_msg;
 extern const pb_msgdesc_t PB_SmartKnobState_msg;
@@ -392,10 +491,15 @@ extern const pb_msgdesc_t PB_SmartKnobConfig_msg;
 extern const pb_msgdesc_t PB_RequestState_msg;
 extern const pb_msgdesc_t PB_PersistentConfiguration_msg;
 extern const pb_msgdesc_t PB_MotorCalibration_msg;
+extern const pb_msgdesc_t PB_StrainState_msg;
+extern const pb_msgdesc_t PB_StrainCalibration_msg;
 
 /* Defines for backwards compatibility with code written before nanopb-0.4.0 */
 #define PB_FromSmartKnob_fields &PB_FromSmartKnob_msg
 #define PB_ToSmartknob_fields &PB_ToSmartknob_msg
+#define PB_Knob_fields &PB_Knob_msg
+#define PB_MotorCalibState_fields &PB_MotorCalibState_msg
+#define PB_StrainCalibState_fields &PB_StrainCalibState_msg
 #define PB_Ack_fields &PB_Ack_msg
 #define PB_Log_fields &PB_Log_msg
 #define PB_SmartKnobState_fields &PB_SmartKnobState_msg
@@ -403,17 +507,24 @@ extern const pb_msgdesc_t PB_MotorCalibration_msg;
 #define PB_RequestState_fields &PB_RequestState_msg
 #define PB_PersistentConfiguration_fields &PB_PersistentConfiguration_msg
 #define PB_MotorCalibration_fields &PB_MotorCalibration_msg
+#define PB_StrainState_fields &PB_StrainState_msg
+#define PB_StrainCalibration_fields &PB_StrainCalibration_msg
 
 /* Maximum encoded size of messages (where known) */
 #define PB_Ack_size                              6
-#define PB_FromSmartKnob_size                    451
+#define PB_FromSmartKnob_size                    399
+#define PB_Knob_size                             134
 #define PB_Log_size                              393
+#define PB_MotorCalibState_size                  2
 #define PB_MotorCalibration_size                 15
 #define PB_PersistentConfiguration_size          28
 #define PB_RequestState_size                     0
 #define PB_SMARTKNOB_PB_H_MAX_SIZE               PB_FromSmartKnob_size
 #define PB_SmartKnobConfig_size                  184
 #define PB_SmartKnobState_size                   206
+#define PB_StrainCalibState_size                 11
+#define PB_StrainCalibration_size                5
+#define PB_StrainState_size                      16
 #define PB_ToSmartknob_size                      196
 
 #ifdef __cplusplus

commit f65504df89b8748044907b8e383a29b514748495

Fixed bugs introduced in commit 288b8e4 (#121)

firmware/src/configuration.cpp
@@ -244,7 +244,10 @@ bool Configuration::saveOSConfigurationInMemory(OSConfiguration os_config)
 
 bool Configuration::saveOSConfiguration(OSConfiguration os_config)
 {
-    EEPROM.put(OS_MODE_EEPROM_POS, os_config.mode);
+    {
+        SemaphoreGuard lock(mutex_);
+        EEPROM.put(OS_MODE_EEPROM_POS, os_config.mode);
+    }
 
     return EEPROM.commit();
 }

firmware/src/main.cpp
@@ -27,7 +27,7 @@ static LedRingTask *led_ring_task_p = &led_ring_task;
 static LedRingTask *led_ring_task_p = nullptr;
 #endif
 
-static MotorTask motor_task(0, config);
+static MotorTask motor_task(1, config);
 
 #if SK_WIFI
 static WifiTask wifi_task(1);
@@ -45,7 +45,7 @@ static MqttTask *mqtt_task_p = nullptr;
 
 #endif
 
-static SensorsTask sensors_task(0, &config);
+static SensorsTask sensors_task(1, &config);
 static SensorsTask *sensors_task_p = &sensors_task;
 
 static ResetTask reset_task(1, config);

firmware/src/root_task.cpp
@@ -162,6 +162,7 @@ void RootTask::run()
                                         OSConfiguration *os_config = this->configuration_->getOSConfiguration();
                                         os_config->mode = os_mode;
                                         this->configuration_->saveOSConfiguration(*os_config);
+                                        this->configuration_->saveOSConfigurationInMemory(*os_config); 
 
                                         switch (os_config->mode)
                                         {
@@ -216,14 +217,17 @@ void RootTask::run()
     // TODO: move playhaptic to notifier? or other interface to just pass "possible" motor commands not entire object/class.
     reset_task_->setMotorTask(&motor_task_);
 
+    configuration_->loadOSConfiguration();
+
     switch (configuration_->getOSConfiguration()->mode)
     {
     case Onboarding:
+        os_config_notifier_.setOSMode(Onboarding);
         display_task_->enableOnboarding();
         break;
     case Demo:
         os_config_notifier_.setOSMode(Onboarding);
-        // display_task_->enableOnboarding();
+        display_task_->enableOnboarding();
         break;
     case Hass:
         display_task_->enableHass();
@@ -323,9 +327,25 @@ void RootTask::run()
             case SK_RESET_BUTTON_PRESSED:
                 app_state.screen_state.awake_until = millis() + 15000;
                 app_state.screen_state.has_been_engaged = true;
+                display_task_->getErrorHandlingFlow()
+                    ->handleEvent(wifi_event);
+                break;
             case SK_RESET_BUTTON_RELEASED:
                 display_task_->getErrorHandlingFlow()
                     ->handleEvent(wifi_event);
+                switch (configuration_->getOSConfiguration()->mode)
+                {
+                case Onboarding:
+                    display_task_->getOnboardingFlow()->triggerMotorConfigUpdate();
+                    break;
+                case Demo:
+                    display_task_->getDemoApps()->triggerMotorConfigUpdate();
+                    break;
+                case Hass:
+                    display_task_->getHassApps()->triggerMotorConfigUpdate();
+                default:
+                    break;
+                }
                 break;
             case SK_MQTT_CONNECTION_FAILED:
             case SK_MQTT_RETRY_LIMIT_REACHED: