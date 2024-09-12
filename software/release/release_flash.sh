6782c1c9bc275730494aaa36e94e62e214767d6c
carlhampuswall <carl.hampus.wall@gmail.com>
2024-09-12 19:23:20 +0200

    Should now work on mac...

--- software/release/release_flash.sh
+++ software/release/release_flash.sh
@@ -2,7 +2,7 @@
 export PATH=$PATH:$HOME/.platformio/penv/bin
 
 #Get pio device port for esp32
-device=$(pio device list | grep -B 2 "VID:PID=303A:1001" | grep -o -E '/dev/(tty|cu)\w+')
+device=$(pio device list | grep -B 2 "VID:PID=303A:1001" | grep -o -E '/dev/(tty|cu)\S*')
 
 echo "Device port: $device"
 
