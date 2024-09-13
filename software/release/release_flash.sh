2c2bd7f2452ee4dded2b661bbe608a1ca1fb7fae
carlhampuswall <carl.hampus.wall@gmail.com>
2024-09-13 20:11:19 +0200

    Cleanup

--- software/release/release_flash.sh
+++ software/release/release_flash.sh
@@ -1,17 +1,6 @@
 #!/bin/bash
 export PATH=$PATH:$HOME/.platformio/penv/bin
 
-#Get pio device port for esp32
-device=$(pio device list | grep -B 2 "VID:PID=303A:1001" | grep -o -E '/dev/(tty|cu)\S*')
-
-if [ -z "$device" ]; then
-    echo "Device not found. Please connect the device and try again."
-    exit 1
-fi
-
-echo "Device port: $device"
-
-
 latest_tag=$(git describe --tags `git rev-list --tags --max-count=1`)
 dist_path="./software/release/dist/$latest_tag/"
 
@@ -38,10 +27,10 @@ else
     echo "Latest release already downloaded."
 fi
 
-# Check if pio is installed
-if ! command -v pio &> /dev/null
+# Check if Platformio is installed
+if ! command -v platformio &> /dev/null
 then
-    echo "pio is not installed."
+    echo "Platformio is not installed."
     read -p "Do you want to install PlatformIO via curl? (y/n): " choice
     case "$choice" in 
       y|Y ) echo "Installing PlatformIO..."
@@ -52,50 +41,56 @@ then
       * ) echo "Invalid input. Please press y or n.";;
     esac
 else
-    echo "pio is already installed."
+    echo "Platformio is already installed."
 fi
+
+echo ""
+
+#Get Platformio device port for esp32
+device=$(platformio device list | grep -B 2 "VID:PID=303A:1001" | grep -o -E '/dev/(tty|cu)\S*')
+
+if [ -z "$device" ]; then
+    echo "Device not found. Please connect the device and try again."
+    exit 1
+fi
+
+echo "Device port: $device"
 
 read -p "Enter order id: " order
 
+echo ""
+
 start=$(date +%s)
-# mac_address=$(python3 ./software/release/get_mac_address.py)
 mac_address=$(platformio device list | grep -B2 'VID:PID=303A:1001' | grep 'SER=' | grep -o 'SER=[^ ]*' | grep -o '[^=]*$')
-echo "[0/4] Saving mac address $mac_address"
+echo "[0/3] Saving mac address $mac_address"
 echo "$order - $mac_address" >> "./software/release/mac_addresses.txt"
 end=$(date +%s)
-echo "[0/4] done in $(($end-$start)) seconds"
+echo "[0/3] done in $(($end-$start)) seconds"
 
 start=$(date +%s)
-echo "[1/4] Erasing flash..."
-platformio run -t erase > /dev/null
+echo "[1/3] Erasing flash..."
+~/.platformio/packages/tool-esptoolpy/esptool.py --chip esp32s3 --port $device --baud 921600 --before default_reset --after hard_reset erase_flash > /dev/null
 end=$(date +%s)
-echo "[1/4] done in $(($end-$start)) seconds"
-
-# start=$(date +%s)
-# echo "[2/4] Flashing filesystem..."
-# platformio run -t upload > /dev/null
-# end=$(date +%s)
-# echo "[2/4] done in $(($end-$start)) seconds"
+echo "[1/3] done in $(($end-$start)) seconds"
 
 start=$(date +%s)
-echo "[3/4] Uploading firmware..."
+echo "[2/3] Uploading firmware..."
 ~/.platformio/packages/tool-esptoolpy/esptool.py --chip esp32s3 --port $device --baud 921600 --before default_reset --after hard_reset write_flash -z --flash_mode dio --flash_freq 80m --flash_size 16MB \
 0x0000 ./software/release/dist/0.4.0-rc1/bootloader.bin \
 0x8000 ./software/release/dist/0.4.0-rc1/partitions.bin \
 0xe000 ./software/release/dist/0.4.0-rc1/boot_app0.bin \
 0x10000 ./software/release/dist/0.4.0-rc1/firmware.bin > /dev/null
-# ~/.platformio/packages/tool-esptoolpy/esptool.py --baud 921600 write_flash -z 0x00010000 $device 0x1000 bootloader.bin \
 end=$(date +%s)
-echo "[3/4] done in $(($end-$start)) seconds"
+echo "[2/4] done in $(($end-$start)) seconds"
 
 start=$(date +%s)
-echo "[4/4] Connect to serial..."
+echo "[3/3] Connect to serial..."
 (while ! test -c $device; do sleep 1; echo 'connecting...'; done; )
 platformio device monitor --baud 115200
 
 deactivate
 end=$(date +%s)
-echo "[4/4] done in $(($end-$start)) seconds"
+echo "[3/3] done in $(($end-$start)) seconds"
 
 echo "Finished in $(($end-$start_0)) seconds"
 
