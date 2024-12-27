15f8217c235becdf9343b173077a987e26f019f4
carlhampuswall <carl.hampus.wall@gmail.com>
2024-12-27 19:31:10 +0100

    Release script updated to new mem address for fatfs.

--- software/release/release_flash.sh
+++ software/release/release_flash.sh
@@ -39,24 +39,24 @@ else
     echo "Latest release already downloaded."
 fi
 
-# Check if Platformio is installed
-# if ! command -v platformio &> /dev/null 
-# then
-#     echo "Platformio is not installed."
-#     read -p "Do you want to install PlatformIO via curl? (y/n): " choice
-#     case "$choice" in 
-#       y|Y ) echo "Installing PlatformIO..."
-#             curl -fsSL -o get-platformio.py https://raw.githubusercontent.com/platformio/platformio-core-installer/master/get-platformio.py
-#             python3 get-platformio.py
-#             echo "PlatformIO installed successfully.";;
-#       n|N ) echo "PlatformIO installation required" 
-#             exit 1;;
-#     * )     echo "Invalid input. Please press y or n."
-#             exit 1;;
-#     esac
-# else
-#     echo "Platformio is already installed."
-# fi
+#Check if Platformio is installed
+if ! command -v platformio &> /dev/null 
+then
+    echo "Platformio is not installed."
+    read -p "Do you want to install PlatformIO via curl? (y/n): " choice
+    case "$choice" in 
+      y|Y ) echo "Installing PlatformIO..."
+            curl -fsSL -o get-platformio.py https://raw.githubusercontent.com/platformio/platformio-core-installer/master/get-platformio.py
+            python3 get-platformio.py
+            echo "PlatformIO installed successfully.";;
+      n|N ) echo "PlatformIO installation required" 
+            exit 1;;
+    * )     echo "Invalid input. Please press y or n."
+            exit 1;;
+    esac
+else
+    echo "Platformio is already installed."
+fi
 
 echo ""
 
@@ -109,19 +109,19 @@ echo ""
 
 start=$(date +%s)
 mac_address=$(platformio device list | grep -B2 'VID:PID=303A:1001' | grep 'SER=' | grep -o 'SER=[^ ]*' | grep -o '[^=]*$')
-echo "[0/6] Saving mac address $mac_address"
+echo "[0/5] Saving mac address $mac_address"
 echo "$order - $mac_address" >> "./software/release/mac_addresses.txt"
 end=$(date +%s)
-echo -e "[0/6] Done in $(($end-$start)) seconds \n"
+echo -e "[0/5] Done in $(($end-$start)) seconds \n"
 
 start=$(date +%s)
-echo "[1/6] Erasing flash..."
+echo "[1/5] Erasing flash..."
 ~/.platformio/packages/tool-esptoolpy/esptool.py --chip esp32s3 --port $device --baud 921600 --before default_reset --after no_reset erase_flash > /dev/null
 end=$(date +%s)
-echo -e "[1/6] Done in $(($end-$start)) seconds\n"
+echo -e "[1/5] Done in $(($end-$start)) seconds\n"
 
 start=$(date +%s)
-echo "[2/6] Uploading recovery firmware..."
+echo "[2/5] Uploading recovery firmware..."
 #find way to have same partitions for recovery and firmware etc
 ~/.platformio/packages/tool-esptoolpy/esptool.py --chip esp32s3 --port $device --baud 921600 --before default_reset --after no_reset write_flash -z --flash_mode dio --flash_freq 80m --flash_size 16MB \
 0x0000 $dist_recovery_path/bootloader.bin \
@@ -129,36 +129,29 @@ echo "[2/6] Uploading recovery firmware..."
 0xe000 $dist_recovery_path/boot_app0.bin \
 0x10000 $dist_recovery_path/firmware.bin > /dev/null
 end=$(date +%s)
-echo -e "[2/6] Done in $(($end-$start)) seconds\n"
+echo -e "[2/5] Done in $(($end-$start)) seconds\n"
 
 start=$(date +%s)
-echo "[3/6] Uploading firmware..."
-~/.platformio/packages/tool-esptoolpy/esptool.py --chip esp32s3 --port $device --baud 921600 --before default_reset --after no_reset write_flash -z --flash_mode dio --flash_freq 80m --flash_size 16MB \
+echo "[3/5] Uploading firmware..."
+~/.platformio/packages/tool-esptoolpy/esptool.py --chip esp32s3 --port $device --baud 921600 --before default_reset --after hard_reset write_flash -z --flash_mode dio --flash_freq 80m --flash_size 16MB \
 0x610000 $dist_path/firmware.bin \
-0xC50000 $dist_path/fatfs.bin
+0xC51000 $dist_path/fatfs.bin > /dev/null #0xC51000 instead of 0xC50000
 end=$(date +%s)
-echo -e "[3/6] Done in $(($end-$start)) seconds\n"
-
-# start=$(date +%s)
-# echo "[4/6] Uploading filesystem..."
-# ~/.platformio/packages/tool-esptoolpy/esptool.py --chip esp32s3 --port $device --baud 921600 --before default_reset --after no_reset write_flash -z --flash_mode dio --flash_freq 80m --flash_size 16MB \
-# 0xC50000 $dist_path/fatfs.bin
-# end=$(date +%s)
-# echo -e "[4/6] Done in $(($end-$start)) seconds\n"
+echo -e "[3/5] Done in $(($end-$start)) seconds\n"
 
 start=$(date +%s)
-echo "[5/6] Switching to release firmware OTA partition..."
+echo "[4/5] Switching to release firmware OTA partition..."
 otatool.py -p $device switch_ota_partition --slot 1
 end=$(date +%s)
-echo -e "[5/6] Done in $(($end-$start)) seconds\n"
+echo -e "[4/5] Done in $(($end-$start)) seconds\n"
 
 
 start=$(date +%s)
-echo "[6/6] Connect to serial..."
+echo "[5/5] Connect to serial..."
 (while ! test -c $device; do sleep 1; echo 'connecting...'; done; )
 platformio device monitor --baud 115200
 end=$(date +%s)
-echo -e "[6/6] Done in $(($end-$start)) seconds\n"
+echo -e "[5/5] Done in $(($end-$start)) seconds\n"
 echo "Finished in $(($end-$start_0)) seconds"
 
 deactivate
